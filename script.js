7be1b6617ad3c5ab41ee60eb98fa5b181a5c1de3
marcioalvesramos90@hotmail.com <marcioalvesramos90@hotmail.com>
2018-12-17 14:46:45 -0200

    creating DOM with javascript

--- script.js
+++ script.js
@@ -1,23 +1,63 @@
 const URL =
   "https://api.vagalume.com.br/rank.php?apikey=660a4395f992ff67786584e238f501aa&type=art&period=week&periodVal=201134&scope=all&limit=3";
 
+var box = document.getElementById("cardBox");
+
 axios
   .get(URL)
   .then(res => {
     return res.data.art.week.all;
   })
   .then(obj => {
-    return getBox(obj);
+    creatItemsBox(obj);
   });
 
-function getBox(obj) {
-  let box = document.getElementById("card-box");
-  let itens = obj.map(item => boxElement(item));
-  return itens;
+function creatItemsBox(items) {
+  // loop para inserir itens no box
+  items.forEach(item => {
+    // Create item
+    let itemDiv = document.createElement("div");
+    // Add class name in item
+    itemDiv.className = "card-item";
+    // Create content
+    let content = document.createElement("div");
+    // Add class name in content
+    content.className = "card-content";
+    // Create img
+    let img = createIMG(item.name, item.pic_medium);
+    // Create title name
+    let nameTitle = createHeaderCard(item.name);
+    // Append img in content
+    content.appendChild(img);
+    // Append content in item
+    itemDiv.appendChild(content);
+    // Append title in item
+    itemDiv.appendChild(nameTitle);
+    // Append Item in Box
+    box.appendChild(itemDiv);
+  });
 }
 
-function boxElement(item) {
-  let itemElement = document.createElement("div");
-  itemElement.className = "card-box";
-  return itemElement;
+function createIMG(name, picture) {
+  let img = document.createElement("img");
+  // Add alt description in img
+  img.alt = name;
+  // Add SRC in img
+  img.src = picture;
+  return img;
 }
+
+function createHeaderCard(name) {
+  let title = document.createElement("div");
+  // add class name
+  title.className = "card-head";
+  // Create P element
+  let parag = document.createElement("p");
+  // Add text node in P element
+  parag.appendChild(document.createTextNode(name));
+  // Append P in title
+  title.appendChild(parag);
+  return title;
+}
+
+function createCardType() {}
